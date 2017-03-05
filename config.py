09b983a21f3fe04d455ff5d1dc561343f3ad54da
sanjayss34 <sanjay_ssubramanian@hotmail.com>
2017-03-04 20:22:32 -0600

    Side panel and other basic UI Changes

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    APP_NAME = 'IdleFreePhilly'
+    APP_NAME = 'VisionZeroPhilly'
     SECRET_KEY = os.environ.get('SECRET_KEY') or \
         'SjefBOa$1FgGco0SkfPO392qqH9%a492'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
