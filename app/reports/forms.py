62b60f13dc3f5de097554d04ed360e8c3de6d0ef
natashanarang <nnarang777@gmail.com>
2017-03-12 18:37:59 -0400

    added form for incident report

--- app/reports/forms.py
+++ app/reports/forms.py
@@ -27,73 +27,43 @@ from .. import db
 
 
 class IncidentReportForm(Form):
-    vehicle_id = StringField('Vehicle ID', validators=[
-        InputRequired('Vehicle ID is required.'),
-        StrippedLength(
-            min_length=1,
-            max_length=15,
-            message='Vehicle ID must be between 1 to 15 characters after '
-                    'removing all non-alphanumeric characters.'
-        ),
-    ])
+    address = StringField('Address', validators=[
+        InputRequired('Address is required.'),
+        ValidLocation()
+        ])
 
-    license_plate = StringField('License Plate Number', validators=[
-        Optional(),
-        StrippedLength(
-            min_length=1,
-            max_length=12,
-            message='License plate must be between 1 to 12 characters after '
-                    'removing all non-alphanumeric characters.'
-        )
-    ])
-
-    bus_number = IntegerField('Bus Number', validators=[
+    automobile_num = IntegerField('Automobile', validators=[
         Optional()
     ])
 
-    led_screen_number = IntegerField('LED Screen Number', validators=[
+    bicycle_num = IntegerField('Bicycle', validators=[
         Optional()
     ])
 
-    latitude = HiddenField('Latitude')
-    longitude = HiddenField('Longitude')
-    location = StringField('Address', validators=[
-        InputRequired(),
-        ValidLocation(),
+    motorcycle_num = IntegerField('Motorcycle', validators=[
+        Optional()
+    ])
 
+    pedestrian_num = IntegerField('Pedestrian', validators=[
+        Optional()
+    ])
+
+    other_num = IntegerField('Other', validators=[
+        Optional()
     ])
 
     today = datetime.datetime.today()
+
     date = DateField('Date (year-month-day)',
                      default=today.strftime('%m-%d-%Y'),
                      validators=[InputRequired()])
+
     time = TimeField('Time (hours:minutes am/pm)',
                      default=today.strftime('%I:%M %p'),
                      validators=[InputRequired()])
 
-    duration = IntegerField('Idling Duration (in minutes)', validators=[
-        InputRequired('Idling duration is required.'),
-        NumberRange(min=0,
-                    message='Idling duration must be positive.')
-    ])
-
-    agency = QuerySelectField(
-        'Vehicle Agency ',
-        validators=[InputRequired()],
-        get_label='name',
-        query_factory=lambda: db.session.query(Agency).filter_by(
-            is_official=True).order_by(Agency.name),
-        allow_blank=True,
-        blank_text='Other',
-    )
-
-    other_agency = StringField(
-        'Other Agency',
-        validators=[RequiredIf(agency=None)]
-    )
-
     picture_file = FileField(
-        'Upload a picture of the idling vehicle.',
+        'Upload a photo (optional)',
         validators=[
             Optional(),
             FileAllowed(['jpg', 'jpe', 'jpeg', 'png', 'gif', 'svg', 'bmp'],
@@ -101,17 +71,20 @@ class IncidentReportForm(Form):
         ]
     )
 
-    picture_url = StringField('Picture URL', validators=[
-        Optional(),
-        URL(message='Picture URL must be a valid URL. '
-                    'Please upload the image to an image hosting website '
-                    'and paste the link here.')
-        ])
-
     description = TextAreaField('Additional Notes', validators=[
+        InputRequired(),
+        Length(max=5000)
+    ])
+
+    injuries = TextAreaField('Injuries (optional)', validators=[
         Optional(),
         Length(max=5000)
     ])
+
+    contact_name = StringField('Contact Name (optional): only visible to administrator', validators=[
+        Optional(),
+        Length(max=1000)
+    ])    
 
     submit = SubmitField('Create Report')
 
