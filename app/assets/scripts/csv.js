fbba64c9f22e7d364976267c5e49f2bc5c59d908
Daniel Zhang <dzhang55@gmail.com>
2017-03-26 17:13:03 -0400

    Modified csv based on new incident report model

--- app/assets/scripts/csv.js
+++ app/assets/scripts/csv.js
@@ -1,23 +1,46 @@
-// Init CSV array.
-var csv = [
-  ['data:text/csv;charset=utf-8,DATE,LOCATION,VEHICLE ID,DURATION,LICENSE PLATE,DESCRIPTION']
-];
+function downloadCSV(isAdmin) {
+    console.log(isAdmin);
+    // Init CSV array.
+    var csv;
+    if (isAdmin) {
+        csv = [
+            ['data:text/csv;charset=utf-8,DATE,LOCATION,PEDESTRIAN NUMBER,AUTOMOBILE NUMBER,' + 
+            'BICYCLE NUMBER,OTHER NUMBER,DESCRIPTION,INJURIES,PICTURE URL,CONTACT NAME,' + 
+            'CONTACT PHONE,CONTACT EMAIL']
+    ];
+    } else {
+        console.log("no admin columns");
+        csv = [
+            ['data:text/csv;charset=utf-8,DATE,LOCATION,PEDESTRIAN NUMBER,AUTOMOBILE NUMBER,' + 
+            'BICYCLE NUMBER,OTHER NUMBER,DESCRIPTION,INJURIES,PICTURE URL']
+        ];
+    }
+    markersDisplayedOnMap.forEach(function(marker) {
+        console.log(isAdmin);
+        var line;
+        if (isAdmin) {
+            console.log("should be true: " + isAdmin);
+            line = [marker.incidentDate, marker.locationName, marker.pedestrianNum,
+                    marker.automobileNum, marker.bicycleNum, marker.otherNum,
+                    marker.description, marker.injuries, marker.pictureUrl,
+                    marker.contactName, marker.contactPhone, marker.contactEmail];
+        } else {
+            console.log("no admin lines");
+            line = [marker.incidentDate, marker.locationName, marker.pedestrianNum,
+                    marker.automobileNum, marker.bicycleNum, marker.otherNum,
+                    marker.description, marker.injuries, marker.pictureUrl];
+        }
 
-function downloadCSV() {
-	console.log('here');
-	markersDisplayedOnMap.forEach(function(marker) {
-		var line = [marker.incidentDate, marker.locationName, marker.vehicleID,
-				    marker.duration, marker.licensePlate, marker.description];
-		csv.push(line.join(','));
-	});
-	var csvString = csv.join('\n');
+        csv.push(line.join(','));
+    });
+    var csvString = csv.join('\n');
 
-	var encodedUri = encodeURI(csvString);
-	var link = document.createElement("a");
-	link.setAttribute("href", encodedUri);
-	link.setAttribute("download", "my_data.csv");
-	document.body.appendChild(link); // Required for FF
+    var encodedUri = encodeURI(csvString);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "my_data.csv");
+    document.body.appendChild(link); // Required for FF
 
-	// Download the data file.
-	link.click();
+    // Download the data file.
+    link.click();
 }
