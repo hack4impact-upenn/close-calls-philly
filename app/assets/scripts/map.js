1eb386f8f4fc68d169a0b618f730dbb199a9a47b
sanjayss34 <sanjay_ssubramanian@hotmail.com>
2017-04-08 01:14:26 -0400

    Polygon filtering is now working\!

--- app/assets/scripts/map.js
+++ app/assets/scripts/map.js
@@ -194,8 +194,12 @@ function initializeDateRange() {
   $('#end-date').calendar('set date', endDate);
 }
 
-function withinBounds(marker, bounds) {
+function withinRectangleBounds(marker, bounds) {
     return bounds === null || bounds.contains(marker.getPosition());
+}
+
+function withinPolygonBounds(marker, vertices) {
+    return vertices === null || google.maps.geometry.poly.containsLocation(marker.getPosition(), polygon);
 }
 
 function withinDateRange(marker) {
@@ -206,9 +210,10 @@ function withinDateRange(marker) {
 function filterMarkers() {
     var markersDisplayedOnMap = [];
     var bounds = (rectangle === null) ? null : rectangle.getBounds();
+    var vertices = (polygon === null) ? null : polygon.getPath();
     for (mw = 0; mw < globalMarkers.length; mw++) {
         var marker = globalMarkers[mw];
-        if (withinBounds(marker, bounds) && withinDateRange(marker)) {
+        if (withinRectangleBounds(marker, bounds) && withinPolygonBounds(marker, vertices) && withinDateRange(marker)) {
             marker.setMap(globalMap);
             markersDisplayedOnMap.push(marker);
         } else {
