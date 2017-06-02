dfbaf44fe362fa8dc0c224389eb9c8e18b58ea64
sanjayss34 <sanjay_ssubramanian@hotmail.com>
2017-06-02 00:47:59 -0500

    Adding print in email function

--- app/email.py
+++ app/email.py
@@ -7,6 +7,7 @@ from app import create_app
 
 def send_email(recipient, subject, template, **kwargs):
     app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+    print('here')
     with app.app_context():
         msg = Message(app.config['EMAIL_SUBJECT_PREFIX'] + ' ' + subject,
                       sender=app.config['EMAIL_SENDER'],
