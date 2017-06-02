5cdbeb94da5e403ab67e0a9cb691a26acdbf6c33
sanjayss34 <sanjay_ssubramanian@hotmail.com>
2017-06-02 01:43:03 -0500

    More email changes

--- app/email.py
+++ app/email.py
@@ -1,12 +1,14 @@
 import os
 from flask import render_template
+from flask import current_app as ap
 from flask_mail import Message
 from . import mail
 from app import create_app
 
 
 def send_email(recipient, subject, template, **kwargs):
-    app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+    print('here1')
+    # app = create_app(os.getenv('FLASK_CONFIG') or 'default')
     print('here')
     with app.app_context():
         msg = Message(app.config['EMAIL_SUBJECT_PREFIX'] + ' ' + subject,
