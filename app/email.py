3d260aa40570c965a3ff31d42413052effed1c75
sanjayss34 <sanjay_ssubramanian@hotmail.com>
2017-06-02 01:52:43 -0500

    updating email to reflect heroku error

--- app/email.py
+++ app/email.py
@@ -1,6 +1,5 @@
 import os
 from flask import render_template
-from flask import current_app as ap
 from flask_mail import Message
 from . import mail
 from app import create_app
@@ -8,7 +7,7 @@ from app import create_app
 
 def send_email(recipient, subject, template, **kwargs):
     print('here1')
-    # app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+    app = create_app(os.getenv('FLASK_CONFIG') or 'default')
     print('here')
     with app.app_context():
         msg = Message(app.config['EMAIL_SUBJECT_PREFIX'] + ' ' + subject,
